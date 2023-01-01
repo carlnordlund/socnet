14517c2a923baaaff51a857f8cb5d8897aa3ea1b
Carl Nordlund <carl.nordlund71@gmail.com>
2023-01-01 20:39:07 +0100

    Dev

---
+++ Socnet.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Socnet - Network analysis in C#");
+
+Socnet.SocnetEngine engine = new Socnet.SocnetEngine();
+
+// If there are no arguments or if argument is  -i or --interactive
+Console.WriteLine("socnet.exe " + String.Join(" ", args));
+
+
+string mode = "interactive";
+bool verbose = false;
+
+if (args == null || args.Length == 0 || args[0].Equals("-i") || args[0].Equals("--interactive"))
+{
+    // Running the engine in interactive mode
+    mode = "interactive";
+}
+else
+{
+    if (args[0].Equals("-f") || args[0].Equals("--file"))
+    {
+        // Running the engine executable with a provided script file
+        if (args.Length == 1)
+        {
+            Console.WriteLine("Error: script <file> missing");
+            return;
+        }
+        else
+        {
+            string filepath = args[1];
+            Console.WriteLine("Starting socnet.exe with script file: " + filepath);
+            if (args.Length == 3 && (args[2].Equals("-v") || args[2].Equals("--verbose")))
+            {
+                Console.WriteLine("Verbose output");
+                verbose = true;
+            }
+            Console.WriteLine("Running script (could take a while)...");
+            List<string> response = engine.loadAndExecuteScript(filepath);
+            Console.WriteLine("Done!");
+            if (verbose)
+            {
+                Console.WriteLine("Output:");
+                foreach (string line in response)
+                    Console.WriteLine(line);
+            }
+        }
+    }
+}
+
+//if (args == null || args.Length == 0)
+//    engine.startInteractiveMode();
+//else
+//{
+//    int index = 0;
+//    while (index < args.Length - 1)
+//    {
+//        Console.WriteLine(args[index]);
+//    }
+//}
+
---
+++ SocnetEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socnet
+{
+    public class SocnetEngine
+    {
+
+        public SocnetEngine()
+        {
+        }
+
+        internal void startInteractiveMode()
+        {
+
+        }
+
+        internal List<string> loadAndExecuteScript(string filepath)
+        {
+            List<string> response = new List<string>();
+            response.Add("1st response line from executing script");
+            response.Add("2nd response line from executing script");
+            return response;
+        }
+    }
+}
