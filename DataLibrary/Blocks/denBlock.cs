e9197e84681f1a77c9d2795e1fe301edfb3f494a
Carl Nordlund <carl.nordlund71@gmail.com>
2023-07-07 03:57:13 +0200

    Implemented proper den(d) block

--- DataLibrary/Blocks/denBlock.cs
+++ DataLibrary/Blocks/denBlock.cs
@@ -10,6 +10,13 @@ namespace Socnet.DataLibrary.Blocks
     {
         public double d;
 
+        public denBlock()
+        {
+            Name = "den";
+            this.d = 0.5;
+            primeIndex = 9;
+        }
+
         public denBlock(double d = 0.5)
         {
             Name = "den";
@@ -17,9 +24,33 @@ namespace Socnet.DataLibrary.Blocks
             primeIndex = 9;
         }
 
+        public override void initArgValue(double v)
+        {
+            this.d = v;
+        }
+
         public override _Block cloneBlock()
         {
             return new denBlock(this.d);
+        }
+
+        public override List<Triple> getTripletList(Matrix matrix, Cluster rowCluster, Cluster colCluster)
+        {
+            List<Triple> triplets = new List<Triple>();
+            List<double> observedList = new List<double>();
+            foreach (Actor rowActor in rowCluster.actors)
+                foreach (Actor colActor in colCluster.actors)
+                    if (rowActor != colActor)
+                        observedList.Add(matrix.Get(rowActor, colActor));
+            int nbrCells = observedList.Count;
+            int i1 = (int)Math.Round((double)nbrCells * d);
+            int i0 = nbrCells - i1;
+            observedList.Sort();
+            for (int i = 0; i < i0; i++)
+                triplets.Add(new Triple(observedList[i], 0, 1));
+            for (int i = i0; i < nbrCells; i++)
+                triplets.Add(new Triple(observedList[i], 1, 1));
+            return triplets;
         }
 
         public override double getPenaltyHamming(Matrix matrix, Cluster rowCluster, Cluster colCluster)
