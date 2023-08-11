145b2f2d418d808393818fee12e7df7d17edcb02
Carl Nordlund <carl.nordlund71@gmail.com>
2023-08-11 17:39:08 +0200

    pco-block initial work

---
+++ DataLibrary/Blocks/pcoBlock.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socnet.DataLibrary.Blocks
+{
+    public class pcoBlock : _Block
+    {
+        public double p;
+
+        public pcoBlock()
+        {
+            Name = "pco";
+            this.p = 0.5;
+            primeIndex = 11;
+        }
+
+        public pcoBlock(double p=0.5)
+        {
+            Name = "pco";
+            this.p = p;
+            primeIndex = 11;
+        }
+
+        public override void initArgValue(double v)
+        {
+            this.p = v;
+        }
+        public override _Block cloneBlock()
+        {
+            return new pcoBlock(this.p);
+        }
+
+
+
+
+
+        public override string ToString()
+        {
+            return Name + "_" + p;
+        }
+
+
+    }
+}
