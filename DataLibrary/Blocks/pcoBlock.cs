ccb548c0faecf60f9fe166ad3ad36311150e2679
Carl Nordlund <carl.nordlund71@gmail.com>
2023-09-28 22:42:08 +0200

    new pco density stuff

--- DataLibrary/Blocks/pcoBlock.cs
+++ DataLibrary/Blocks/pcoBlock.cs
@@ -33,8 +33,66 @@ namespace Socnet.DataLibrary.Blocks
             return new pcoBlock(this.p);
         }
 
+        //public override List<Triple> getTripletList(Matrix matrix, Cluster rowCluster, Cluster colCluster)
+        //{
+        // Original approach, using wcc approach
+        //    List<Triple> triplets = new List<Triple>();
+        //    // nr, nc : Nbr of rows/columns
+        //    double nr = rowCluster.actors.Count, nc = colCluster.actors.Count;
+
+        //    // kr, kc : Nbr of cells to check in each row/column
+        //    double kr = Math.Ceiling((nc - ((rowCluster == colCluster ? 1 : 0))) * p), kc = Math.Ceiling((nr - ((rowCluster == colCluster ? 1 : 0))) * p);
+
+        //    // nt : Total number of triplets that will be checked
+        //    double nt = (int)kr * nr + (int)kc * nc;
+
+        //    // n : Total number of cells in the block
+        //    double n = nr * (nc - ((rowCluster == colCluster) ? 1 : 0));
+
+        //    // w : Weight per triplet so that total weight is size of block
+        //    double w = n / nt;
+
+        //    // rowLists: dict with row values for each of the row actors
+        //    // colLists: corresponding storage of column values, for each column actor
+        //    Dictionary<Actor, List<double>> rowLists = new Dictionary<Actor, List<double>>();
+        //    Dictionary<Actor, List<double>> colLists = new Dictionary<Actor, List<double>>();
+
+        //    // Prepare dictionaries
+        //    foreach (Actor rowActor in rowCluster.actors)
+        //        rowLists.Add(rowActor, new List<double>());
+        //    foreach (Actor colActor in colCluster.actors)
+        //        colLists.Add(colActor, new List<double>());
+        //    double v;
+        //    foreach (Actor rowActor in rowCluster.actors)
+        //        foreach (Actor colActor in colCluster.actors)
+        //            if (rowActor!=colActor)
+        //            {
+        //                v = matrix.Get(rowActor, colActor);
+        //                rowLists[rowActor].Add(v);
+        //                colLists[colActor].Add(v);
+        //            }
+        //    foreach (KeyValuePair<Actor, List<double>> rowObj in rowLists)
+        //    {
+        //        rowObj.Value.Sort();
+        //        rowObj.Value.Reverse();
+        //        for (int k = 0; k < kr; k++)
+        //            triplets.Add(new Triple(rowObj.Value[k], 1, w)); // Fix weight here
+
+        //    }
+
+        //    foreach (KeyValuePair<Actor, List<double>> colObj in colLists)
+        //    {
+        //        colObj.Value.Sort();
+        //        colObj.Value.Reverse();
+        //        for (int k = 0; k < kc; k++)
+        //            triplets.Add(new Triple(colObj.Value[k], 1, w));
+        //    }
+        //    return triplets;
+        //}
+
         public override List<Triple> getTripletList(Matrix matrix, Cluster rowCluster, Cluster colCluster)
         {
+            // Non-weighted approach: simply correlate remaining with themselves
             List<Triple> triplets = new List<Triple>();
             // nr, nc : Nbr of rows/columns
             double nr = rowCluster.actors.Count, nc = colCluster.actors.Count;
@@ -43,13 +101,10 @@ namespace Socnet.DataLibrary.Blocks
             double kr = Math.Ceiling((nc - ((rowCluster == colCluster ? 1 : 0))) * p), kc = Math.Ceiling((nr - ((rowCluster == colCluster ? 1 : 0))) * p);
 
             // nt : Total number of triplets that will be checked
-            double nt = (int)kr * nr + (int)kc * nc;
+            //double nt = (int)kr * nr + (int)kc * nc;
 
             // n : Total number of cells in the block
             double n = nr * (nc - ((rowCluster == colCluster) ? 1 : 0));
-
-            // w : Weight per triplet so that total weight is size of block
-            double w = n / nt;
 
             // rowLists: dict with row values for each of the row actors
             // colLists: corresponding storage of column values, for each column actor
@@ -64,7 +119,7 @@ namespace Socnet.DataLibrary.Blocks
             double v;
             foreach (Actor rowActor in rowCluster.actors)
                 foreach (Actor colActor in colCluster.actors)
-                    if (rowActor!=colActor)
+                    if (rowActor != colActor)
                     {
                         v = matrix.Get(rowActor, colActor);
                         rowLists[rowActor].Add(v);
@@ -75,7 +130,9 @@ namespace Socnet.DataLibrary.Blocks
                 rowObj.Value.Sort();
                 rowObj.Value.Reverse();
                 for (int k = 0; k < kr; k++)
-                    triplets.Add(new Triple(rowObj.Value[k], 1, w)); // Fix weight here
+                    triplets.Add(new Triple(rowObj.Value[k], 1, 0.5)); // Fix weight here
+                for (int k = (int)Math.Ceiling(kr); k < rowObj.Value.Count; k++)
+                    triplets.Add(new Triple(rowObj.Value[k], rowObj.Value[k], 0.5)); // Fix weight here
 
             }
 
@@ -84,11 +141,12 @@ namespace Socnet.DataLibrary.Blocks
                 colObj.Value.Sort();
                 colObj.Value.Reverse();
                 for (int k = 0; k < kc; k++)
-                    triplets.Add(new Triple(colObj.Value[k], 1, w));
+                    triplets.Add(new Triple(colObj.Value[k], 1, 0.5));
+                for (int k = (int)Math.Ceiling(kc); k < colObj.Value.Count; k++)
+                    triplets.Add(new Triple(colObj.Value[k], colObj.Value[k], 0.5));
             }
             return triplets;
         }
-
 
 
         public override string ToString()
